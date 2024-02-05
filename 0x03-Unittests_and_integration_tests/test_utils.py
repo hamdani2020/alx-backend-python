195c60ed0b140d724b72b85a120aae84e0ec0b93
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-05 21:35:42 +0000

    Test passed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -53,7 +53,7 @@ class TestGetJson(unittest.TestCase):
     Test for get_json function
     """
     @parameterized.expand([
-        ("http://example.com", {"payload": True}),
+        ("http://lusitech.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(
@@ -68,3 +68,30 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attributes)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    This is test for memoize function
+    """
+    def test_memoize(self) -> None:
+        """
+        Test for memoize output
+        """
+        class TestClass:
+            def a_method(self):
+                """ Method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
